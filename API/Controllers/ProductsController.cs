d8fb305165619ee4f790be91ab23cc11c1049c76
Alex Greene <greene.alexanderj@gmail.com>
2020-06-21 14:49:29 -0400

    fix warnings

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,9 +2,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
-using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
