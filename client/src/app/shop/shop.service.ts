e5d5602518fe110d41cde585799c75cf1d3aa64a
Alex Greene <greene.alexanderj@gmail.com>
2020-06-27 22:50:00 -0400

    introduce page routing add product details page

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import {IProductBrand} from "../shared/models/productBrand";
 import {IProductType} from "../shared/models/productType";
 import {map} from "rxjs/operators";
 import {ShopParams} from "../shared/models/shopParams";
+import {IProduct} from "../shared/models/product";
 
 @Injectable({
   providedIn: 'root'
@@ -39,7 +40,10 @@ export class ShopService {
           return response.body;
         })
       )
+  }
 
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   getBrands() {
