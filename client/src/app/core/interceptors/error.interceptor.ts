d26f189e21ec66210d508feecceed00a4b7aadd2
Alex Greene <greene.alexanderj@gmail.com>
2020-07-08 08:59:40 -0400

    do error messaging things

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -7,22 +7,27 @@ import {ToastrService} from "ngx-toastr";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private router: Router, private toastr: ToastrService) {}
+  constructor(private router: Router, private toastr: ToastrService) {
+  }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError(error => {
-        if(error){
-          if(error.status === 400){
+        if (error) {
+          if (error.status === 400) {
+            if (error.error.errors) {
+              throw error.error;
+            } else {
+              this.toastr.error(error.error.message, error.error.statusCode)
+            }
+          }
+          if (error.status === 401) {
             this.toastr.error(error.error.message, error.error.statusCode)
           }
-          if(error.status === 401){
-            this.toastr.error(error.error.message, error.error.statusCode)
-          }
-          if(error.status === 404) {
+          if (error.status === 404) {
             this.router.navigateByUrl('/not-found');
           }
-          if(error.status === 500) {
+          if (error.status === 500) {
             this.router.navigateByUrl('/server-error');
           }
         }
